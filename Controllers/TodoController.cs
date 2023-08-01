27e77c03caa659479cc455836452ffbf615a48f8
Lucas Augusto <lucasagdeassis@gmail.com>
2023-08-01 00:36:53 -0300

    Initial commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoAPI.Models;
+
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/<TodoController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetAll()
+        {
+            var response = await _context.Todos.ToListAsync();
+
+            return Ok(response);
+        }
+
+        // GET api/<TodoController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> GetById(int id)
+        {
+            var response = await _context.Todos.FindAsync(id);
+
+            if (response == null) { return NotFound(); }
+
+            return Ok(response);
+        }
+
+        // POST api/<TodoController>
+        [HttpPost]
+        public async Task<ActionResult<Todo>> AddTodo(Todo todo)
+        {
+            _context.Todos.Add(todo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = todo.Id }, todo);
+        }
+
+        // PUT api/<TodoController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Todo>> CompletedTodo(int id, Todo todo)
+        {
+            if (id != todo.Id) { return BadRequest(); }
+
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return Ok(todo);
+        }
+
+        // DELETE api/<TodoController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null) { return NotFound(); }
+
+            _context.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoExists(int id)
+        {
+            return _context.Todos.Any(x => x.Id == id);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using TodoAPI.Models;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*");
+                      });
+});
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<TodoContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
